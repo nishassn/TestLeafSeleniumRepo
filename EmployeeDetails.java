12832f76fabb4e3bcfc8a67e96622891aba3d6c5
nishassn <nisha.ssn@gmail.com>
2025-10-12 06:13:47 +0530

    Add files via upload
    Week 1 Day 1 Home Assignments - Employee details Java file has been uploaded

---
+++ EmployeeDetails.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Declare and initialize the variables
+		
+		String name = "John Doe";
+		byte age = 30;
+		float salary = 55000.75f;
+		char gender = 'M';
+		boolean acceptTerms = true;
+		float rating = 4.5f;
+		long mobileNumber = 9876543210L;
+		int pinCode = 56001;
+		byte luckyNumber = 7;
+		
+		//Print all the variables
+		
+		System.out.println("****Employee Details***");
+		System.out.println("Name: "+name+"\n"+"Age: "+age+"\n"+"Salary: "+salary+"\n"+"Gender: "+gender);
+		System.out.println("Accept Terms: "+acceptTerms+"Rating: "+rating+"\nMobile Number: "+mobileNumber);
+		System.out.println("Pin Code: "+pinCode+"\nLucky Number: "+luckyNumber);
+	}
+
+}
